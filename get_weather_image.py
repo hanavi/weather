e547d372a8e5bab53a8944447a44a0c25e53e748
James Casey <hanavi@gmail.com>
2019-09-06 13:16:29 -0500

    more windows support

--- get_weather_image.py
+++ get_weather_image.py
@@ -7,29 +7,37 @@ from bs4 import BeautifulSoup as bs
 import os
 import tempfile
 
+# Gets rid of InsecureRequestWarning that crops up due to BCBSAL Proxy
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 image_url = re.compile(".*\(([^)]+)\).*")
 site_url = "https://weather.com/weather/today/l/USAL0054:1:US"
 
-req = requests.get(site_url)
+req = requests.get(site_url,verify=False)
 soup = bs(req.content, features='lxml')
 
 divs = soup.find("div", attrs={'id': 'ts-0'})
 url_match = image_url.match(str(divs.get('style')))
 
-dl = requests.get(url_match.group(1))
+dl = requests.get(url_match.group(1), verify=False)
 
 # with open('image.jpg', 'wb') as fd:
 #     fd.write(dl.content)
 
-tmpfile = tempfile.mkstemp(suffix='.jpg', prefix="/tmp/weather_")
+if os.name == "nt":
+    tmpfile = tempfile.mkstemp(suffix='.jpg', prefix=r"\temp\weather_")
+else:
+    tmpfile = tempfile.mkstemp(suffix='.jpg', prefix="/tmp/weather_")
+
 fd = tmpfile[0]
 fname = tmpfile[1]
 
 os.write(fd, dl.content)
 os.close(fd)
 
-os.system(f"feh {fname}")
-
-
+if os.name == "nt":
+    os.system(f"{fname}")
+else:
+    os.system(f"feh {fname}")
 
